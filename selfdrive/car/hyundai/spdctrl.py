32e899f76c5adddde5f38f6f83712f53afcf2ba2
durango-op <71580184+durango-op@users.noreply.github.com>
2020-12-02 21:21:39 +0900

    Update spdctrl.py

--- selfdrive/car/hyundai/spdctrl.py
+++ selfdrive/car/hyundai/spdctrl.py
@@ -15,7 +15,7 @@ EventName = car.CarEvent.EventName
 class Spdctrl(SpdController):
     def __init__(self, CP=None):
         super().__init__( CP )
-        self.cv_Raio = 0.2
+        self.cv_Raio = 0.3
         self.cv_Dist = -5
         self.steer_mode = ""
         self.cruise_gap = 0.0
@@ -53,7 +53,7 @@ class Spdctrl(SpdController):
             vRel2 = int(vRelef) # for cut-in detection??
 
         dst_lead_distance = int(CS.clu_Vanz*self.cv_Raio)   # 기준 유지 거리
-        dst_lead_distance2 = int(CS.clu_Vanz*0.2)   # 기준 유지 거리
+        dst_lead_distance2 = int(CS.clu_Vanz*0.3)   # 기준 유지 거리
         
         if dst_lead_distance > 100:
             dst_lead_distance = 100
@@ -131,13 +131,13 @@ class Spdctrl(SpdController):
         elif self.cruise_set_speed_kph > CS.clu_Vanz:  #이온설정속도가 차량속도보다 큰경우
             if lead_objspd > 5 and CS.clu_Vanz < 15 and CS.VSetDis < 40: # 처음출발시 선행차량 급가속할 때 차량속도 20되기 전 최대한 설정속도 업 한 후 대기
                 self.seq_step_debug = "SS>VS,초가"
-                lead_wait_cmd, lead_set_speed = self.get_tm_speed( CS, 10, 5)
+                lead_wait_cmd, lead_set_speed = self.get_tm_speed( CS, 5, 5)
             elif lead_objspd > 5 and CS.clu_Vanz > 40 and CS.VSetDis < 60: # 처음출발시 선행차량 급가속할 때 차량속도 20되기 전 최대한 설정속도 업 한 후 대기
                 self.seq_step_debug = "SS>VS,중가"
-                lead_wait_cmd, lead_set_speed = self.get_tm_speed( CS, 10, 5)
+                lead_wait_cmd, lead_set_speed = self.get_tm_speed( CS, 5, 5)
             elif lead_objspd > 5 and CS.clu_Vanz > 60 and CS.VSetDis < 80: # 처음출발시 선행차량 급가속할 때 차량속도 20되기 전 최대한 설정속도 업 한 후 대기
                 self.seq_step_debug = "SS>VS,종가"
-                lead_wait_cmd, lead_set_speed = self.get_tm_speed( CS, 10, 5)
+                lead_wait_cmd, lead_set_speed = self.get_tm_speed( CS, 5, 5)
             elif lead_objspd >= 0 and CS.clu_Vanz >= (int(CS.VSetDis) - 5) and int(CS.clu_Vanz * 0.5) < dRel:
                 self.seq_step_debug = "SS>VS,+1"
                 lead_wait_cmd, lead_set_speed = self.get_tm_speed( CS, 75, 5)
